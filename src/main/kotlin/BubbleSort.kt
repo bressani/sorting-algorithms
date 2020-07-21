144954d4f03acf6eb25c8eae54bce63e379eb682
bressani <jr.bressani@gmail.com>
2020-07-21 09:01:26 -0300

    Improves bubbleSort

--- src/main/kotlin/BubbleSort.kt
+++ src/main/kotlin/BubbleSort.kt
@@ -1,14 +1,16 @@
 fun main() {
     val array = intArrayOf(3, 2, 6, 0, 5, 20, 1, 4, 0, 0, 0, 0, 1, 1, 1, 25, 25, 25, 2, 1, 3)
 
-    bubbleSort(array)
+    val sortedArray = bubbleSort(array)
 
-    array.map { a -> print("${a}, ") }
+    sortedArray.map { a -> print("${a}, ") }
 }
 
-fun bubbleSort(array: IntArray) {
+fun bubbleSort(intArray: IntArray): IntArray {
+    val array = intArray.copyOf()
+
     do {
-        var hasSwapped = false;
+        var hasSwapped = false
         for (index in array.indices) {
             if (index == (array.size - 1))
                 break
@@ -24,9 +26,15 @@ fun bubbleSort(array: IntArray) {
 
     } while (hasSwapped)
 
+    return array
+
 }
 
 fun swapWithNext(array: IntArray, currentPosition: Int) {
+    if (array.size - 2 < currentPosition) {
+        return
+    }
+
     val temp: Int = array[currentPosition]
     array[currentPosition] = array[currentPosition + 1]
     array[currentPosition + 1] = temp
