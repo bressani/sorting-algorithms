7d89381cd17f2f0034670c1f1e4a48ea169ce38d
bressani <jr.bressani@gmail.com>
2021-04-16 21:44:40 -0300

    Adds merge sort

---
+++ src/main/kotlin/MergeSort.kt
@@ -0,0 +1,44 @@
+fun main() {
+    val array = intArrayOf(3, 2, 6, 0, 5, 20, 1, 4, 0, 0, 0, 0, 1, 1, 1, 25, 25, 25, 2, 1, 3)
+
+    val sortedArray = mergeSort(array)
+
+    sortedArray.map { element -> print("${element}, ") }
+}
+
+fun mergeSort(intArray: IntArray): IntArray {
+    var array = intArray.copyOf()
+
+    do {
+        var hasSwapped = false
+        for (index in array.indices) {
+            if (index == (array.size - 1))
+                break
+
+            val nextNumber: Int = array[index + 1]
+
+            if (array[index] > nextNumber) {
+                array = swapWithNextIndex(array, index)
+                hasSwapped = true
+            }
+        }
+
+    } while (hasSwapped)
+
+    return array
+}
+
+fun swapWithNextIndexe(intArray: IntArray, currentPosition: Int): IntArray {
+    val array = intArray.copyOf()
+
+    val minArraySizeToBeSwapped = array.size - 2
+    if (minArraySizeToBeSwapped < currentPosition) {
+        return array
+    }
+
+    val temp: Int = array[currentPosition]
+    array[currentPosition] = array[currentPosition + 1]
+    array[currentPosition + 1] = temp
+
+    return array
+}
